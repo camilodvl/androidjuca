4276b8e3214a07a103e1c72015c8fb1dc632d543
camilodvl <camilo.228@hotmail.com>
2022-09-05 21:08:37 -0500

    Actividad registro

--- app/src/main/java/co/edu/iudigital/actividad1/MainActivity.java
+++ app/src/main/java/co/edu/iudigital/actividad1/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -14,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         TimerTask tarea = new TimerTask() {
             @Override
             public void run() {
@@ -24,5 +27,6 @@ public class MainActivity extends AppCompatActivity {
         };
         Timer tiempo = new Timer();
         tiempo.schedule(tarea,4000);
+
     }
 }
--- app/src/main/java/co/edu/iudigital/actividad1/login.java
+++ app/src/main/java/co/edu/iudigital/actividad1/login.java
@@ -2,13 +2,29 @@ package co.edu.iudigital.actividad1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class login extends AppCompatActivity {
-
+public class login extends AppCompatActivity{
+    Button registrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        registrar=(Button)findViewById(R.id.registrar);
+        registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i= new Intent(view.getContext(), Registro.class);
+                startActivity(i);
+
+            }
+        });
+
     }
 }
