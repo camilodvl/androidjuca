47deb85868cc03792f6660710d292c15e615128e
camilodvl <camilo.228@hotmail.com>
2022-09-08 21:29:27 -0500

    Agregando validaciones de campos

--- app/src/main/java/co/edu/iudigital/actividad1/login.java
+++ app/src/main/java/co/edu/iudigital/actividad1/login.java
@@ -7,16 +7,23 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class login extends AppCompatActivity{
     Button registrar; //Se crea el objeto tipo botón
+    EditText usuario, clave;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        usuario = findViewById(R.id.txtUsuario);
+        clave = findViewById(R.id.txtClave);
 
 
-        registrar=(Button)findViewById(R.id.registrar); //busca el botón por id
+
+        registrar=(Button)findViewById(R.id.btnRegistrar); //busca el botón por id
         registrar.setOnClickListener(new View.OnClickListener() { //agrega una accion al evento onclick
             @Override
             public void onClick(View view) {
@@ -26,5 +33,15 @@ public class login extends AppCompatActivity{
             }
         });
 
+
+    }
+
+    public void Iniciar (View view){
+        String user= usuario.getText().toString();
+        String pass= clave.getText().toString();
+
+        if(user.equals("")||pass.equals("")){
+            usuario.setError("Ingrese un usuario");
+        }
     }
 }
