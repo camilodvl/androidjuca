a7309eb24b3aa3c0c6b5fb7b347acf7869b1dfa8
camilodvl <camilo.228@hotmail.com>
2022-09-14 21:17:01 -0500

    Validando campos en el registro

--- app/src/main/java/co/edu/iudigital/actividad1/Registro.java
+++ app/src/main/java/co/edu/iudigital/actividad1/Registro.java
@@ -6,13 +6,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class Registro extends AppCompatActivity {
+
+    EditText usuario, password, email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+        usuario = findViewById(R.id.txtUsuarioRegistro);
+        password = findViewById(R.id.txtPasswordRegistro);
+        email = findViewById(R.id.txtEmailRegistro);
+    }
 
+    public void GuardarRegistro(View view) {
+        String usuarioV = usuario.getText().toString();
+        String passwordV = password.getText().toString();
+        String emailV = email.getText().toString();
+
+        if (usuarioV.equals("")) {
+            usuario.setError("Ingrese el usuario");
+        } else if (passwordV.equals("")) {
+            password.setError("Ingrese el password");
+        } else if (emailV.equals("")) {
+            email.setError("Ingrese el email");
+        }
     }
 }
--- app/src/main/java/co/edu/iudigital/actividad1/login.java
+++ app/src/main/java/co/edu/iudigital/actividad1/login.java
@@ -9,39 +9,38 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class login extends AppCompatActivity{
+public class login extends AppCompatActivity {
     Button registrar; //Se crea el objeto tipo botón
     EditText usuario, clave;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         usuario = findViewById(R.id.txtUsuario);
         clave = findViewById(R.id.txtClave);
 
-
-
-        registrar=(Button)findViewById(R.id.btnRegistrar); //busca el botón por id
+        registrar = (Button) findViewById(R.id.btnRegistrar); //busca el botón por id
         registrar.setOnClickListener(new View.OnClickListener() { //agrega una accion al evento onclick
             @Override
             public void onClick(View view) {
-                Intent i= new Intent(view.getContext(), Registro.class); //getcontext obtiene la actividad actual
+                Intent i = new Intent(view.getContext(), Registro.class); //getcontext obtiene la actividad actual
                 startActivity(i);
 
             }
         });
 
-
     }
+    //se crea la funcion a ejecutar por el botón ingresar, esta función se asigna al botón en el diseño, al evento onClick
+    public void Iniciar(View view) {
+        String user = usuario.getText().toString();
+        String pass = clave.getText().toString();
 
-    public void Iniciar (View view){
-        String user= usuario.getText().toString();
-        String pass= clave.getText().toString();
-
-        if(user.equals("")||pass.equals("")){
+        if (user.equals("")) {
             usuario.setError("Ingrese un usuario");
+        } else if (pass.equals("")) {
+            clave.setError("Ingrese la clave");
         }
     }
 }
